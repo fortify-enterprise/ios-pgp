8c8724290302d27c01fab1b77b3fb52657362c03
Andrei Odeski <andrei@fortify.pro>
2017-09-03 21:09:27 -0700

    Initial version

---
+++ pgp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pgp
+//
+//  Created by Andrei Odeski on 2016-08-02.
+//  Copyright © 2016 Fortify Communications Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
